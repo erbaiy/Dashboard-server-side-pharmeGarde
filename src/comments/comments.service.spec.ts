3a585fc54548d7d8693a1b837b5e9ec9e2081ec2
youness <younesserbai@gmail.com>
2025-02-04 16:22:59 +0100

    fixed  all the test failed cases for the the pipline

--- src/comments/comments.service.spec.ts
+++ src/comments/comments.service.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsService } from './comments.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comment } from '../comments/comments.schema';
 
 describe('CommentsService', () => {
   let service: CommentsService;
+  let model: Model<Comment>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentsService],
+      providers: [
+        CommentsService,
+        {
+          provide: getModelToken('Comment'),
+          useValue: {
+            new: jest.fn().mockResolvedValue({}),
+            constructor: jest.fn().mockResolvedValue({}),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          }
+        },
+      ],
     }).compile();
 
     service = module.get<CommentsService>(CommentsService);
+    model = module.get<Model<Comment>>(getModelToken('Comment'));
   });
 
   it('should be defined', () => {
--- src/pharmacy/pharmacy.controller.spec.ts
+++ src/pharmacy/pharmacy.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PharmacyController } from './pharmacy.controller';
+import { PharmacyService } from './pharmacy.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('PharmacyController', () => {
   let controller: PharmacyController;
@@ -7,6 +9,19 @@ describe('PharmacyController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PharmacyController],
+      providers: [
+        PharmacyService,
+        {
+          provide: getModelToken('Pharmacy'),
+          useValue: {
+            new: jest.fn().mockResolvedValue({}),
+            constructor: jest.fn().mockResolvedValue({}),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn()
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<PharmacyController>(PharmacyController);
